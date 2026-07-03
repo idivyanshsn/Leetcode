d52af082502969b6202c03f7e18d85291e1d24db
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-07-03 12:21:41 +0530

    Time: 0 ms (100%), Space: 12.8 MB (47.5%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,21 +11,10 @@
  */
 class Solution {
 public:
-    void function(TreeNode* root,vector<int>& result){
-        if(root==nullptr){
-            result.push_back(INT_MIN);
-            return;
-        }
-        result.push_back(root->val);
-        function(root->left,result);
-        function(root->right,result);
-
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int> parr;
-        vector<int> qarr;
-        function(p,parr);
-        function(q,qarr);
-        return parr==qarr;
+        if(p==nullptr || q==nullptr){
+            return (p==q);
+        }
+        return (p->val == q->val)&& isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
