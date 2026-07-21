bd5152ce341cc32e22636add98f221de0ae0be8e
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-07-21 19:55:47 +0530

    Time: 0 ms (100%), Space: 7.9 MB (73.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp) {
-        if (n <= 2)
-            return n;
-        if (dp[n] != -1)
-            return dp[n];
-        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-        return dp[n];
-    }
     int climbStairs(int n) {
-        if (n <= 2) {
-            return n;
+        int prev=1;
+        int prev2=0;
+        int curr=0;
+        for(int i=0;i<n;i++){
+            curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
         }
-        vector<int> dp(n + 1, -1);
-        return solve(n, dp);
+        return prev;
     }
 };
