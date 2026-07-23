27055f1e18a84e52cb4c5ae7d12bbd9f5a67d265
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-07-23 12:58:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (74.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int prev=1;
         int prev2=0;
+        int prev=1;
         int curr=0;
         for(int i=0;i<n;i++){
-            curr=prev+prev2;
+            curr=prev2+prev;
             prev2=prev;
             prev=curr;
         }
