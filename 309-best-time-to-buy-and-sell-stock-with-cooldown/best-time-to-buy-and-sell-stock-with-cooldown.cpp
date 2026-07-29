227fceb61f64654e7376b7741f44877e853c856b
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-07-30 01:13:50 +0530

    Time: 7 ms (7.42%) | Memory: 16.1 MB (64.72%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                if(j)
+                    dp[i][j]=max(dp[i+1][1],-prices[i]+dp[i+1][0]);
+                else
+                    dp[i][j]=max(dp[i+1][0],prices[i]+dp[i+2][1]);
+            }
+        }
+        return dp[0][1];
+    }
+};
