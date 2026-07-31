a2cdaa7290d40cfc26a625f6b39986ac9c130ecf
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-07-31 23:51:27 +0530

    Time: 4 ms (73.24%), Space: 18.9 MB (6.93%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int left = 0;
-        int size = 0;
-        unordered_set<char> mpp;
-        for (int right = 0; right < s.size(); right++) {
-            if (mpp.contains(s[right])) {
-                while (mpp.contains(s[right])) {
-                    mpp.erase(s[left]);
-                    left++;
-                }
-            }
-            mpp.insert(s[right]);
-            size = max(size, right - left + 1);
+        int hasharr[256];
+        for(int i =0;i<256;i++){
+            hasharr[i]=-1;
         }
-        return size;
+        int right=0;
+        int left=0;
+        int maxlen=0;
+        int n = s.size();
+        while(right<n){
+            if(hasharr[s[right]]!=-1){
+                left=max(hasharr[s[right]]+1,left);
+            }
+            int len=right-left+1;
+            maxlen=max(maxlen,len);
+            hasharr[s[right]]=right;
+            right++;
+        }
+        return maxlen;
     }
 };
