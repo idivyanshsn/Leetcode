a19dadfec1a89123198e2259b32773005aa64976
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-01 01:38:47 +0530

    Time: 3 ms (67.93%), Space: 11 MB (42.21%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26);
+        int right=0;
+        int left=0;
+        int n = s.size();
+        int ans=0;
+        int maxFreq=0;
+        int maxLen=0;
+        while(right<n){
+            freq[s[right]-'A']++;
+            maxFreq=max(maxFreq,freq[s[right]-'A']);
+            while ((right - left + 1) - maxFreq > k){
+                freq[s[left]-'A']--;
+                left++;
+            }
+            maxLen=max(maxLen,right-left+1);
+            right++;
+            
+        }
+        return maxLen;
+    }
+};
