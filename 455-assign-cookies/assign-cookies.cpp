aa4a6edba6c096b83daaab68a0e905adb6b65e79
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-01 21:15:13 +0530

    Time: 3 ms (94.51%) | Memory: 44.8 MB (48.87%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int n=g.size();
+        int m = s.size();
+        int count=0;
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(g[i]<=s[j]){
+                count++;
+                i++;
+                j++;
+            }
+            else if(g[i]>s[j]){
+                j++;
+            }
+        }
+        return count;
+    }
+};
