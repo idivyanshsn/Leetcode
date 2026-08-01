eae17d952887d31ef3b74d9bcd866744e10de916
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-01 19:40:07 +0530

    Time: 0 ms (100%), Space: 32.6 MB (83.45%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    int function(vector<int>& nums, int goal) {
+        int n = nums.size();
+        if (goal < 0)
+            return 0;
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        int sum = 0;
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+            while (sum > goal) {
+                sum -= nums[left];
+                left++;
+            }
+            count += right - left + 1;
+        }
+        return count;
+    }
+
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return function(nums, goal) - function(nums, goal - 1);
+    }
+};
