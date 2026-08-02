0a37d3011f0df742b8dd8e7373db256f5d8b571b
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-02 17:07:12 +0530

    Time: 79 ms (15.34%) | Memory: 21.8 MB (14.44%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -19,8 +19,16 @@ private:
 
 public:
     int jump(vector<int>& nums) {
+
         int n=nums.size();
-        vector<int> dp(n, -1);
-        return function(nums, 0, dp);
+        vector<int> dp(n, INT_MAX);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            for(int j=1;j<=nums[i] && i+j<n;j++){
+                if(dp[j+i]!=INT_MAX)
+                    dp[i]=min(dp[i],1+dp[i+j]);
+            }
+        }
+        return dp[0];
     }
 };
