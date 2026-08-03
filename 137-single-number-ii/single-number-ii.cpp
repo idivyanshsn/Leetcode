f06b21dbf1d5a703a6781c11fa5576d7450f4750
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-04 00:32:24 +0530

    Time: 3 ms (31.57%) | Memory: 13.3 MB (87.53%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mpp;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
+        int ans = 0;
+        int n = nums.size();
+        for (int i = 0; i < 32; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] & (1 << i))
+                    count++;
+            }
+            if (count % 3)
+                ans |= (1 << i);
         }
-        for(auto it:mpp){
-            if(it.second==1)
-                return it.first;
-        }
-        return 0;
+        return ans;
     }
 };
