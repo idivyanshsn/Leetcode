579de330d2410a76e7a4ea57d5b8e7e817367fb3
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-03 22:15:45 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (60.55%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int res = start ^ goal;
+        int count=0;
+        for (int i = 0; i < 32; i++) {
+            if (res & 1)
+                count++;
+            res = res >> 1;
+        }
+        return count;
+    }
+};
