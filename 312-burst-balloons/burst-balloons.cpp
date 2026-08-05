b0740b1b09d792fcb395f366ccdcd7c2b321f195
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-05 22:49:25 +0530

    Time: 96 ms (76.43%) | Memory: 13.8 MB (83.76%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = n - 2; i >= 1; i--) {
+            for (int j = i; j <= n-2; j++) {
+                int maxi = INT_MIN;
+                for (int k = i; k <= j; k++) {
+                    int coins = nums[i - 1] * nums[k] * nums[j+1] + dp[i][k-1] +
+                                dp[k + 1][j];
+                    maxi = max(coins, maxi);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n - 2];
+    }
+};
