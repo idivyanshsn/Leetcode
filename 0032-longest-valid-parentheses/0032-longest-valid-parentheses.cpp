4aef8e027cd911ccd4b7fa2a16399ed28ff892d4
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-06 19:42:29 +0530

    Time: 3 ms (39.61%), Space: 11.8 MB (24.31%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        stack<int> st;
+        st.push(-1);
+        int length = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.empty())
+                    st.push(i);
+                else {
+                    int currentLength = i - st.top();
+                    length = max(length, currentLength);
+                }
+            }
+        }
+        return length;
+    }
+};
