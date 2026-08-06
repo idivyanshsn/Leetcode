a750ffb9214e18bf96ff2e21cdd7e6e29323fcfd
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-06 17:34:57 +0530

    Time: 0 ms (100%), Space: 8.3 MB (82.52%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char> st;
+        int open = 0, close = 0;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(' ) {
-                st.push(s[i]);
+            if (s[i] == '(')
+                open++;
+            else {
+                if (open > 0)
+                    open--;
+                else
+                    close++;
             }
-            else
-                if (!st.empty()  && st.top()=='(' ) {
-                    st.pop();
-                }
-                else 
-                    st.push(s[i]);
         }
-        return st.size();
+        return open+close;
     }
 };
