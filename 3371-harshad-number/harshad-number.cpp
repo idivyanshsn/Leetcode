ad001def22ee2cbf6e44457c6736f34fb522897e
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-15 20:02:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (50.00%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int n = x;
+        while (x > 0) {
+            sum += x % 10;
+            x = x / 10;
+        }
+        if (n % sum == 0)
+            return sum;
+        return -1;
+    }
+};
