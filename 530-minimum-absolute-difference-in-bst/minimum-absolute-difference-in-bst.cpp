5f8d0f79e34ed40414eff6ced8e23baf3eb99064
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-31 00:03:32 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (22.30%) - LeetSync

---
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void function(TreeNode* root,vector<int>& result){
+        if(root==nullptr) return;
+        function(root->left,result);
+        result.push_back(root->val);
+        function(root->right,result);
+    }
+    int getMinimumDifference(TreeNode* root) {
+        vector<int> result;
+        function(root,result);
+        int minDiff=INT_MAX;
+        for(int i=1;i<result.size();i++){
+            int diff=result[i]-result[i-1];
+            minDiff=min(minDiff,diff);
+        }
+        return minDiff;
+    }
+};
