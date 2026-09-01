51f399d582356ca4bbe3c40336b5ac03fa80f680
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-09-01 18:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (20.60%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return function(root)!=-1;
+    }
+    int function(TreeNode* root){
+        if (root==nullptr)
+            return 0;
+        int left = function(root->left);
+        if(left==-1) return -1;
+        int right = function(root->right);
+        if(right == -1) return -1;
+
+        if(abs(left-right)>1) return -1;
+        return 1+max(left,right);
+    }
+};
