dd315d6013a76a22b93cfba12548868116adeee3
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-09-06 22:59:09 +0530

    Time: 8 ms (60.97%), Space: 35.1 MB (42.14%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-void markSafe(vector<vector<int>>& grid, int i, int j, int m, int n) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0)
-            return;
-        grid[i][j] = 0;
-        markSafe(grid, i + 1, j, m, n);
-        markSafe(grid, i, j + 1, m, n);
-        markSafe(grid, i - 1, j, m, n);
-        markSafe(grid, i, j - 1, m, n);
+    void function(vector<vector<int>>& grid,int i,int j,int n, int m){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]==0) return;
+        grid[i][j]=0;
+        function(grid,i+1,j,n,m);
+        function(grid,i-1,j,n,m);
+        function(grid,i,j-1,n,m);
+        function(grid,i,j+1,n,m);
     }
     int numEnclaves(vector<vector<int>>& grid) {
-        int m = grid.size();
-        if(m==0) return 0;
-        int n = grid[0].size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                bool isSafe = (i == 0 || j == 0 || i == m - 1 || j == n - 1);
-                if (isSafe &&grid[i][j] == 1) {
-                    markSafe(grid, i, j, m, n);
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                bool canWalk=(i==0||j==0||i==n-1||j==m-1);
+                if(canWalk && grid[i][j]==1){
+                    function(grid,i,j,n,m);
                 }
             }
-        }
-        int var =0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    grid[i][j] = 1;
-                } else if (grid[i][j] == 1) {
-                    var++;
-                }
+        }int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1)
+                    count++;
             }
         }
-        return var;
-        
+        return count;
     }
 };
