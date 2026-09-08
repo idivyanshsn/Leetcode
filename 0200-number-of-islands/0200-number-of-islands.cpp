c9f61d29090f950cadf309a7ae5b49b6c44c763e
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-09-08 11:48:51 +0530

    Time: 21 ms (91.76%), Space: 16.3 MB (80.3%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,39 +1,25 @@
 class Solution {
-private:
-    void dfs(int row, int col, vector<vector<int>>& visited,
-             vector<pair<int, int>>& vec, vector<vector<char>>& grid, int row0,
-             int col0) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vec.push_back({row - row0, col - col0});
-        visited[row][col] = 1;
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, 1, 0, -1};
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
-                !visited[nrow][ncol] && grid[nrow][ncol] == '1') {
-                dfs(nrow, ncol, visited, vec, grid, row0, col0);
-            }
-        }
-    }
-
 public:
+    void dfs(vector<vector<char>>& grid,int i, int j,int m,int n){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0') return;
+        grid[i][j]='0';
+        dfs(grid,i+1,j,m,n);
+        dfs(grid,i-1,j,m,n);
+        dfs(grid,i,j+1,m,n);
+        dfs(grid,i,j-1,m,n);
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> visited(m, vector<int>(n, 0));
-        vector<vector<pair<int, int>>> st;
+        int m = grid.size();
+        int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    vector<pair<int, int>> vec;
-                    dfs(i, j, visited, vec, grid, i, j);
-                    st.push_back(vec);
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j, m, n);
                 }
             }
         }
-        return st.size();
+        return count;
     }
 };
