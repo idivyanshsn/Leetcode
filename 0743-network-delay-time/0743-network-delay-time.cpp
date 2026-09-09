7bd2e764faabbe7fd819b9a68e7acebeed312306
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-09-09 17:52:35 +0530

    Time: 90 ms (35.33%), Space: 47.8 MB (36.23%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n+1);
-        for (auto it : times) {
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for (auto it : times)
             adj[it[0]].push_back({it[1], it[2]});
-        }
-        priority_queue<pair<int,int>,
-               vector<pair<int,int>>,
-               greater<pair<int,int>>> q;
-        vector<int> dist(n+1, 1e9);
+
+        vector<int> dist(n + 1, 1e9);
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
         dist[k] = 0;
-        q.push({0, k});
-        while (!q.empty()) {
-            auto it = q.top();
-            q.pop();
-            int dis = it.first;
-            int node = it.second;
+        pq.push({0, k});
+        while (!pq.empty()) {
+            auto [dis, node] = pq.top();
+            pq.pop();
             if (dis > dist[node])
                 continue;
             for (auto it : adj[node]) {
-                int adjnode = it.first;
-                int wt = it.second;
-                if (dis + wt < dist[adjnode]) {
-                    dist[adjnode] = dis + wt;
-                    q.push({dist[adjnode], adjnode});
+                int adjNode = it.first;
+                int adjWt = it.second;
+                if (adjWt + dis < dist[adjNode]) {
+                    dist[adjNode] = adjWt + dis;
+                    pq.push({dist[adjNode], adjNode});
                 }
             }
         }
         int ans = 0;
         for (int i = 1; i <= n; i++) {
-            if (dist[i] == 1e9) {
+            if (dist[i] == 1e9)
                 return -1;
-            }
             ans = max(ans, dist[i]);
         }
         return ans;
