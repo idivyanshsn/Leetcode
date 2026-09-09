433a5f851f30795f1d5e0e8ed4fe9f5a85dd1688
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-09-09 19:28:21 +0530

    Time: 19 ms (14.32%), Space: 37.4 MB (31.46%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -3,33 +3,34 @@ public:
     int countPaths(int n, vector<vector<int>>& roads) {
         vector<vector<pair<int, int>>> adj(n);
         for (auto it : roads) {
-            adj[it[0]].push_back({it[1], it[2]});
-            adj[it[1]].push_back({it[0], it[2]});
+            adj[it[0]].push_back({it[2], it[1]});
+            adj[it[1]].push_back({it[2], it[0]});
         }
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater < pair < long long,
-            int >>> pq;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
+                       greater<pair<long long, int>>>
+            pq;
         vector<long long> dist(n, 1e18);
         vector<int> ways(n, 0);
+        const int mod = 1e9 + 7;
         dist[0] = 0;
         ways[0] = 1;
         pq.push({0, 0});
-        int mod = 1e9 + 7;
         while (!pq.empty()) {
-            long long distance = pq.top().first;
-            int node = pq.top().second;
+            auto [distance, node] = pq.top();
             pq.pop();
-            for (auto it : adj[node]) {
-                long long edWeight = it.second;
-                int adjNode = it.first;
-                if (distance + edWeight < dist[adjNode]) {
-                    dist[adjNode] = distance + edWeight;
-                    pq.push({edWeight + distance, adjNode});
-                    ways[adjNode] = ways[node];
-                } else if (distance + edWeight == dist[adjNode]) {
-                    ways[adjNode] = (ways[adjNode]+ways[node])%mod;
+            if (distance > dist[node])
+                continue;
+            for (auto [edweight, ednode] : adj[node]) {
+                long long newdist = distance + edweight;
+                if (newdist < dist[ednode]) {
+                    dist[ednode] = newdist;
+                    ways[ednode] = ways[node];
+                    pq.push({newdist, ednode});
+                } else if (newdist == dist[ednode]) {
+                    ways[ednode] = (ways[ednode] + ways[node]) % mod;
                 }
             }
         }
-        return ways[n - 1] % mod;
+        return ways[n - 1];
     }
 };
